d9c207f5fc899fb195b4ba1642e3fbf673c5b5c8
Phil Watson <pw.cmp@optusnet.com.au>
2014-10-22 23:30:35 +1000

    Created

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# makeCacheMatrix - Creates a list of handler functions to  cache a matrix inverse
+#                   Takes a square matrix as parameter
+# cacheSolve - Returns a cached matrix inverse
+#              Takes a list of handler functions from makeCacheMatrix
+#
 
-## Write a short comment describing this function
-
+# Returns a list of functions to store and retrieve a cached matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  matinv <- NULL
+  setmat <- function(y) {
+    x <<- y
+    matinv <<- NULL
+  }
+  getmat <- function() x
+  setmatinv <- function(inv) matinv <<- inv
+  getmatinv <- function() matinv
+  
+  list(setmat=setmat, getmat=getmat, setmatinv=setmatinv, getmatinv=getmatinv)
 }
 
 
-## Write a short comment describing this function
-
+# Returns the inverse of matrix from cache or recalculates 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matinv <- x$getmatinv()
+  if (!is.null(matinv)) {
+    message("getting cached inverse")
+    return(matinv)
+  }
+  temp <- x$getmat()
+  matinv <- solve(temp)
+  x$setmatinv(matinv)
+  matinv
 }
